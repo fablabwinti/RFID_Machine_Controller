a5fe9d8164786239cfcf9603c1e56d32dc9039aa
DedeHai <daedae@gmx.ch>
2017-06-11 10:56:58 +0200

    led support and global variable added

--- Firmware/Fablab_RFID_control/output.h
+++ Firmware/Fablab_RFID_control/output.h
@@ -1,13 +1,22 @@
 #define OUTPUTPIN 15 //GPIO15
 
+//user verified, release the machine for use
 void releaseMachine(void)
 {
   digitalWrite(OUTPUTPIN, HIGH);
+  machineLocked = false;
+  LEDColor.h = 0; //red
+  userStarttime = getRtcTimestamp();
+
+  //todo: add sound here?
 }
 
 void lockMachine(void)
 {
   digitalWrite(OUTPUTPIN, LOW);
+  machineLocked = true;
+  LEDColor.h = 87; //green
+  //todo: add sound here?
 }
 
 void initOutput(void)
