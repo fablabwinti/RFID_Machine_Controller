024bd0f9c42afcf30dd7d92f58e0c8ecac7777d2
DedeHai <daedae@gmx.ch>
2017-06-11 10:51:08 +0200

    sound support for final version
    changed pin from debug to final, added login and logout sound, changed
    function names

--- Firmware/Fablab_RFID_control/sound.h
+++ Firmware/Fablab_RFID_control/sound.h
@@ -1,25 +1,33 @@
 
 
 //output pin
-
-#define SOUNDPIN 15//debug!!! use pin 3 //3 //GPIO3 = RX pin
+#define SOUNDPIN 3 //GPIO3 = RX pin
 
 void playTone(int _pin, int _frequency, int _length){
+  pinMode(_pin, OUTPUT);
   analogWriteFreq(_frequency);
   analogWrite(_pin, 512);
   delay(_length);
-  analogWrite(_pin, 0);
+  digitalWrite(SOUNDPIN,LOW);
 }
 
 // playTone(3,2000,1000); //play a tone on USART RX pin
 
-void playSuccess(void)
+void playLogin(void)
 {
   playTone(SOUNDPIN,880,150);
   playTone(SOUNDPIN,1760,100);
 }
 
-void playFail(void)
+
+void playLogout(void)
+{
+  
+  playTone(SOUNDPIN,1760,150);
+  playTone(SOUNDPIN,880,100);
+}
+
+void playDenied(void)
 {
   playTone(SOUNDPIN,784,200);
   playTone(SOUNDPIN,587,200);
